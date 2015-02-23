d844a5c513b9d2263273e46915ec286b05cec1a7
. <linbrek@gmail.com>
2015-02-23 15:36:43 -0500

    A lot cleaner

--- new.js
+++ new.js
@@ -1,5 +1,12 @@
+var cancelEventAction = function(evt)
+{
+    evt.preventDefault();
+};
+
 var dropEventHandler = function(evt)
 {
+    // If length is zero, there are no files and this loop won't occur
+    // If only reading one file, checking length is necessary
     for(var i = 0; i < evt.dataTransfer.files.length; i++)
     {
         loadData(evt.dataTransfer.files[i]);
@@ -7,58 +14,107 @@ var dropEventHandler = function(evt)
     evt.preventDefault();
 };
 
-var cancelEventAction = function(evt)
-{
-    evt.preventDefault();
-};
-
 window.addEventListener("dragover", cancelEventAction);
 window.addEventListener("drop", dropEventHandler);
-
-var ErrorReport = {};
-
-var readData = function(evt)
-{
-    var data = new Uint8Array(evt.target.result);
-    for(str = "", i = 0; i < 11; i++)
-    {
-        str += String.fromCharCode(data[i]);
-    }
-    if(str == "123-456-STD")
-    {
-        data = data.subarray(0x1040);
-    }
-    
-    if(checksumValid(0x20, data))
-    {
-        ErrorReport.Count = 0;
-        var MemPak = readMemPak(data);
-        
-        if(ErrorReport.Count)
-        {
-            console.log(ErrorReport, evt.target.name);
-        } else
-        {
-            console.log(MemPak);
-        }
-    }
-};
 
 var loadData = function(file)
 {
     var reader    = new FileReader();
     reader.name   = file.name;
     reader.onload = readData;
+    // For DexDrive we have to read more than 32768 bytes
     reader.readAsArrayBuffer(file.slice(0, 36928));
+};
+
+var readData = function(evt)
+{
+    var data = new Uint8Array(evt.target.result);
+    
+    // Detect and remove DexDrive headers
+    for(str = "", i = 0; i < 11; i++)
+    {
+        str += String.fromCharCode(data[i]);
+        if(str === "123-456-STD")
+        {
+            data = data.subarray(0x1040);
+        }
+    }
+    
+    // Don't do anything unless checksum is valid
+    if(checksumValid(0x20, data))
+    {
+        var MemPak = readMemPak(data, evt.target.name);
+    }
+};
+
+var readMemPak = function(data, f)
+{
+    /*
+    When loading a file
+        1. If primary is invalid, check and restore from backup
+    
+    When performing a modification or saving
+        1. Update indexTable checksum
+        2. Update backup
+        3. Resize file to 32768
+    */
+    
+    var noteTable   = parseNoteTable(data);
+    var indexTable  = readIndexTable(data, false);
+    var indexTable2 = readIndexTable(data, true);
+    
+    if(indexTable.error.count > 0 && indexTable2.error.count === 0)
+    {
+        /*
+            indexTable_1 is invalid, but indexTable_2 is valid.
+            TODO: Restore the backup data, and proceed.
+        */
+    }
+    
+    var ErrorReport = {
+        "types" : noteTable.error.types.concat(indexTable.error.types),
+        "count" : noteTable.error.count + indexTable.error.count
+    };
+    
+    if(indexTable.noteCount !== noteTable.noteCount)
+    {
+        addError("NoteCountMismatch", ErrorReport);
+    }
+    
+    if(allNotesExist(noteTable.indexes, indexTable.indexes) === false)
+    {
+        addError("NoteDoesNotExist", ErrorReport);
+    }
+    
+    if(ErrorReport.count > 0)
+    {
+        console.log("Error: ", f, ErrorReport.types);
+    }
+    
+    console.log("Success - what should be returned?");
+    return {
+        "Notes": noteTable,
+        "Pages": indexTable
+    };
+};
+
+var addError = function(errorName, errorReport)
+{
+    if(errorReport.types.indexOf(errorName) === -1)
+    {
+        errorReport.types.push(errorName);
+    }
+    errorReport.count++;
 };
 
 var checksumValid = function(o, data)
 {
+    // X,Y = stored checksum -- A,B = calculated checksum
     var sumX  = (data[o + 28] << 8) + data[o + 29],
         sumY  = (data[o + 30] << 8) + data[o + 31],
         sumA  = 0,
         sumB  = 0xFFF2;
-
+        
     for(var i = 0; i < 28; i += 2)
     {
         sumA += (data[o + i] << 8) + data[o + i + 1];
@@ -77,13 +133,33 @@ var checksumValid = function(o, data)
     return (sumX === sumA && sumY === sumB);
 };
 
-var readMemPak = function(data)
+var allNotesExist = function(fileIndexes, pageIndexes)
 {
-    // File Table ******************************************************
+    // Check if Note indexes exist in indexTable
+    for(var i = 0, testPassed = false; i < 16; i++)
+    {
+        if(fileIndexes.sort().toString() === pageIndexes.sort().toString())
+        {
+            testPassed = true;
+        }
+    }
     
-    var fileTable = {"numUsed": 0},
-        indexes   = [];
-    
+    return testPassed;
+};
+
+var parseNoteTable = function(data)
+{
+    var Parser = {
+        "noteCount": 0,
+        "error": {
+            "types":[],
+            "count": 0
+            },
+        "indexes": [],
+        "noteTable": {}
+        };
+        
+    // Loop over NoteTable
     for(var i = 0x300; i < 0x500; i += 32)
     {
         var p = data[i + 0x07], a = data[i + 0x06],
@@ -91,41 +167,51 @@ var readMemPak = function(data)
             
         if(p >= 5 && p <= 127 && a === 0 && b === 0 && c === 0)
         {
-            if (indexes.indexOf(p) !== -1)
+            if (Parser.indexes.indexOf(p) !== -1)
             {
-                ErrorReport.Count++;
-                ErrorReport.DuplicateFileFound = true;
+                addError("DuplicateFileFound", Parser.error);
             }
             
-            indexes.push(p);
+            Parser.indexes.push(p);
+            Parser.noteCount++;
             
-            fileTable[(i - 0x300) / 32] = {
+            Parser.noteTable[(i - 0x300) / 32] = {
                 "initialIndex": p
             };
-            fileTable.numUsed++;
         }
     }
-    
-    // Index Table *****************************************************
-    // TODO: Make into it's own function with offset
-    
-    var a = [],
+    return Parser;
+};
+
+var readIndexTable = function(data, readBackup)
+{
+    var o = readBackup ? 0x200 : 0x100,
+        a = [],
         b = [],
-        usedPages = 0;
-    
-    for(var i = 0x10A; i < 0x200; i += 2)
+        usedPages = 0,
+        Parser = {
+            "noteCount": 0,
+            "error": {
+                "types":[],
+                "count": 0
+                },
+            "indexes": [],
+            "Inodes": {"pageCount" : 0}
+        };
+        
+    // Check the IndexTable
+    for(var i = o + 0xA; i < o + 0x100; i += 2)
     {
         var p  = data[i + 1],
             p2 = data[i];
             
         if(p2 !== 0 || p !== 1 && p !== 3 && p < 5 || p > 127)
         {
-            ErrorReport.IndexCorrupt = true;
-            ErrorReport.Count++;
+            addError("IndexTableCorrupt", Parser.error);
         }
-        else if(p === 1 || p !== 3 && p >= 5 && p <= 127)
+        if(p === 1 || p !== 3 && p >= 5 && p <= 127)
         {
-            a.push((i - 0x100) / 2);
+            a.push( (i - o) / 2 );
             b.push(p);
             usedPages++;
         }
@@ -135,25 +221,23 @@ var readMemPak = function(data)
     {
         return b.indexOf(n) === -1;
     });
-
-    // Parse Index Sequences
     
-    var foundFiles = {},
-        pageCount  = 0;
+    // Parse Index Sequences
+    Parser.noteCount = keyPages.length, 
+    Parser.indexes   = keyPages;
     
     for(var i = 0; i < keyPages.length; i++)
     {
-        var p        = keyPages[i],
-            indexes  = [],
-            foundEnd = false;
-            c        = 0;
-        
+        var indexes  = [],
+            foundEnd = false,
+            c        = 0,
+            p        = keyPages[i];
+            
         while (p === 1 || p >= 5 && p <= 127 && c <= usedPages)
         {
             if (indexes.indexOf(p) !== -1)
             {
-                ErrorReport.infiniteLoop = true;
-                ErrorReport.Count++;
+                addError("InfiniteLoopInSequence", Parser.error);
                 break;
             }
             if(p === 1)
@@ -163,53 +247,26 @@ var readMemPak = function(data)
             }
             indexes.push(p);
             
-            p = data[ (p * 2) + 0x100 + 1 ];
+            p = data[ (p * 2) + o + 1 ];
             c++;
         }
         
         if(foundEnd === true)
         {
-            foundFiles[keyPages[i]] = indexes;
-            pageCount += foundFiles[keyPages[i]].length;
+            Parser.Inodes[keyPages[i]] = indexes;
+            Parser.Inodes.pageCount += indexes.length;
         }
         else
         {
-            ErrorReport.EndNotFound = true;
-            ErrorReport.Count++;
+            addError("NoEndInSequence", Parser.error);
             break;
         }
     }
     
-    // Check if the pageCount matches the calculated usedPages
-    if(usedPages !== pageCount)
+    if(usedPages !== Parser.Inodes.pageCount)
     {
-        ErrorReport.PageCountMismatch = true;
-        ErrorReport.Count++;
-    }
-    else
-    {
-        foundFiles.numUsed = pageCount;
+        addError("PageMismatchInSequence", Parser.error);
     }
     
-    // Check if the number of notes are the same
-    if(keyPages.length !== fileTable.numUsed)
-    {
-        ErrorReport.FileCountMismatch = true;
-        ErrorReport.Count++;
-    }
-    
-    // Check if Note indexes exist in keyPages
-    for(var i = 0; i < 16; i++)
-    {
-        if(fileTable[i] && keyPages.indexOf(fileTable[i].initialIndex) === -1)
-        {
-            ErrorReport.MissingNoteIndex = true;
-            ErrorReport.Count++;
-        }
-    }
-    
-    return {
-        "Notes": fileTable,
-        "Pages": foundFiles
-    };
-}
+    return Parser;
+};
