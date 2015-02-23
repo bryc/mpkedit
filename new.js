fd5cde4d9a4fb868c7ff83fa948d377383075572
. <linbrek@gmail.com>
2015-02-23 01:22:56 -0500

    Update new.js

--- new.js
+++ new.js
@@ -1,144 +1,216 @@
-var dragIsFile = false;
-
-window.addEventListener("dragenter", function(e) {
-    var types = e.dataTransfer.types;
-    for(var i = 0, q = false; i<types.length; i++) {
-        if(types[i] == "Files") {q = true;}
-    }
-    dragIsFile = q;
-});
-
-window.addEventListener("dragover", function(e) {
-    e.preventDefault();
-    if(!dragIsFile) {
-        e.dataTransfer.dropEffect = "none";
-    }
-});
-
-window.addEventListener("drop", function(e) {
-    e.preventDefault();
-    var fl = e.dataTransfer.files;
-    for(var i = 0; i < fl.length; i++) {
-        var f = new FileReader();
-        f.readAsArrayBuffer(fl[i].slice(0,36928));
-        f.name = fl[i].name;
-        f.onload = function(e) {
-            var d = new Uint8Array(e.target.result);
-            for(var i = 0, q = ""; i < 11; i++) {
-                q += String.fromCharCode(d[i]);
-            }
-            if(q == "123-456-STD") {d = d.subarray(4160);}
-            // Do the ID Area check here.
-            if(isIDValid(0x20, d)){
-                ReadMemPak(d, e.target.name);
-            }
-        };
-    }
-});
-
-function isIDValid(o, data)
+var dropEventHandler = function(evt)
 {
-    for(var i = 0, sum = 0; i < 28; i += 2, sum &= 0xFFFF)
+    for(var i = 0; i < evt.dataTransfer.files.length; i++)
     {
-        sum += (data[o + i] << 8) + data[o + i + 1];
+        loadData(evt.dataTransfer.files[i]);
     }
-    var sum2 = 0xFFF2 - sum,
-        sumA = (data[o + 28] << 8) + data[o + 29],
-        sumB = (data[o + 30] << 8) + data[o + 31];
-    
-    if(sumA == sum && sumB != sum2 && sumB == sum2 ^ 0x0C)
+    evt.preventDefault();
+};
+
+var cancelEventAction = function(evt)
+{
+    evt.preventDefault();
+};
+
+window.addEventListener("dragover", cancelEventAction);
+window.addEventListener("drop", dropEventHandler);
+
+var ErrorReport = {};
+
+var readData = function(evt)
+{
+    var data = new Uint8Array(evt.target.result);
+    for(str = "", i = 0; i < 11; i++)
     {
-        sum2 ^= 0x0C;
-        data[o + 31] ^= 0x0C;
+        str += String.fromCharCode(data[i]);
     }
-    return (sumA == sum && sumB == sum2);
-}
-
-function ReadMemPak(d, _f) {
-
-    var errors = {count: 0};
+    if(str == "123-456-STD")
+    {
+        data = data.subarray(0x1040);
+    }
     
-    // FILE TABLE ---------------------------------------
-    var fileTable = {}, out=[];
-    for(var i = 0x300, _c=0; i < 0x500; i+=32) {
-        var p = d[i+7],  a = d[i+6],
-            b = d[i+10], c = d[i+11];
-        if(p>=5 && p<=127 && a==0 && b==0 && c==0) {
-            if (out.indexOf(p) !== -1) {
-                errors.DuplicateNoteFound = true;
-                errors.count++;
+    if(checksumValid(0x20, data))
+    {
+        ErrorReport.Count = 0;
+        var MemPak = readMemPak(data);
+        
+        if(ErrorReport.Count)
+        {
+            console.log(ErrorReport, evt.target.name);
+        } else
+        {
+            console.log(MemPak);
+        }
+    }
+};
+
+var loadData = function(file)
+{
+    var reader    = new FileReader();
+    reader.name   = file.name;
+    reader.onload = readData;
+    reader.readAsArrayBuffer(file.slice(0, 36928));
+};
+
+var checksumValid = function(o, data)
+{
+    var sumX  = (data[o + 28] << 8) + data[o + 29],
+        sumY  = (data[o + 30] << 8) + data[o + 31],
+        sumA  = 0,
+        sumB  = 0xFFF2;
+
+    for(var i = 0; i < 28; i += 2)
+    {
+        sumA += (data[o + i] << 8) + data[o + i + 1];
+        sumA &= 0xFFFF;
+        
+    }
+    
+    sumB -= sumA;
+    
+    // Repair corrupt DexDrive checksums
+    if(sumX === sumA && sumY !== sumB)
+    {
+        sumY ^= 0xC;
+        data[o + 31] ^= 0xC;
+    }
+    
+    return (sumX === sumA && sumY === sumB);
+};
+
+var readMemPak = function(data)
+{
+    // File Table ******************************************************
+    
+    var fileTable = {"numUsed": 0},
+        indexes   = [];
+    
+    for(var i = 0x300; i < 0x500; i += 32)
+    {
+        var p = data[i + 0x07], a = data[i + 0x06],
+            b = data[i + 0x0A], c = data[i + 0x0B];
+            
+        if(p >= 5 && p <= 127 && a === 0 && b === 0 && c === 0)
+        {
+            if (indexes.indexOf(p) !== -1)
+            {
+                ErrorReport.Count++;
+                ErrorReport.DuplicateFileFound = true;
             }
-            out.push(p);
-            fileTable[(i-0x300)/32] = {
+            
+            indexes.push(p);
+            
+            fileTable[(i - 0x300) / 32] = {
                 "initialIndex": p
             };
-            _c++;
+            fileTable.numUsed++;
         }
     }
-    fileTable.numUsed = _c;
     
-    // INDEX TABLE ---------------------------------------
-    var _a=[], _b=[], usedPages = 0;
-    for(var i = 0x10A; i < 0x200; i += 2) {
-        var p = d[i+1], p2 = d[i];
-        if((p2 != 0) || (p != 1 && p != 3 && p < 5 || p > 127)) {
-            errors.IndexCorrupt = true;
-            errors.count++;
+    // Index Table *****************************************************
+    // TODO: Make into it's own function with offset
+    
+    var a = [],
+        b = [],
+        usedPages = 0;
+    
+    for(var i = 0x10A; i < 0x200; i += 2)
+    {
+        var p  = data[i + 1],
+            p2 = data[i];
+            
+        if(p2 !== 0 || p !== 1 && p !== 3 && p < 5 || p > 127)
+        {
+            ErrorReport.IndexCorrupt = true;
+            ErrorReport.Count++;
         }
-        else if(p!=3 && p>=5 && p<=127 || p==1) {
-            _a.push((i - 0x100) / 2);
-            _b.push(p);
+        else if(p === 1 || p !== 3 && p >= 5 && p <= 127)
+        {
+            a.push((i - 0x100) / 2);
+            b.push(p);
             usedPages++;
         }
     }
-    //startingIndexes
-    var start = _a.filter(function(n){return _b.indexOf(n) == -1;});
     
-    // FILE TABLE: PARSE SEQUENCES ------------------
-    var foundFiles = {}, pageCount = 0;
-    for(var i = 0 ; i < start.length; i++) {
-        var p = start[i];
-        var output = [];
-        var foundEnd = false;
-        var y = 0;
-        while (p==1 || p>=5 && p<=127 && y <= usedPages) {
-            if (output.indexOf(p) !== -1) {
-                errors.infiniteLoop = true;
-                errors.count++; break;
-            }
-            if(p == 1) {
-                foundEnd = true; break;
-            }
-            output.push(p);
-            p = d[ (p * 2) + 0x100 + 1 ];
-            y++;
-        }
-        if(foundEnd === true) {
-            foundFiles[start[i]] = output;
-            pageCount += foundFiles[start[i]].length;
-        }
-        else {
-                errors.EndNotFound = true;
-                errors.count++; break;
-        }
-    }
-    if(usedPages !== pageCount) {
-        errors.PageCountMismatch = true;
-        errors.count++;
-    }
-    if(start.length !== fileTable.numUsed) {
-        errors.FileCountMismatch = true;
-        errors.count++;
-    }
-    for(var i = 0; i < 16; i++){
-        if(fileTable[i] && start.indexOf(fileTable[i].initialIndex) == -1) {
-            errors.MissingIndex = true;
-            errors.count++;
-        }
-    }
-    
-    if(errors.count===0)
+    var keyPages = a.filter(function(n)
     {
-        console.log(fileTable, foundFiles, _f);
+        return b.indexOf(n) === -1;
+    });
+
+    // Parse Index Sequences
+    
+    var foundFiles = {},
+        pageCount  = 0;
+    
+    for(var i = 0; i < keyPages.length; i++)
+    {
+        var p        = keyPages[i],
+            indexes  = [],
+            foundEnd = false;
+            c        = 0;
+        
+        while (p === 1 || p >= 5 && p <= 127 && c <= usedPages)
+        {
+            if (indexes.indexOf(p) !== -1)
+            {
+                ErrorReport.infiniteLoop = true;
+                ErrorReport.Count++;
+                break;
+            }
+            if(p === 1)
+            {
+                foundEnd = true;
+                break;
+            }
+            indexes.push(p);
+            
+            p = data[ (p * 2) + 0x100 + 1 ];
+            c++;
+        }
+        
+        if(foundEnd === true)
+        {
+            foundFiles[keyPages[i]] = indexes;
+            pageCount += foundFiles[keyPages[i]].length;
+        }
+        else
+        {
+            ErrorReport.EndNotFound = true;
+            ErrorReport.Count++;
+            break;
+        }
     }
+    
+    // Check if the pageCount matches the calculated usedPages
+    if(usedPages !== pageCount)
+    {
+        ErrorReport.PageCountMismatch = true;
+        ErrorReport.Count++;
+    }
+    else
+    {
+        foundFiles.numUsed = pageCount;
+    }
+    
+    // Check if the number of notes are the same
+    if(keyPages.length !== fileTable.numUsed)
+    {
+        ErrorReport.FileCountMismatch = true;
+        ErrorReport.Count++;
+    }
+    
+    // Check if Note indexes exist in keyPages
+    for(var i = 0; i < 16; i++)
+    {
+        if(fileTable[i] && keyPages.indexOf(fileTable[i].initialIndex) === -1)
+        {
+            ErrorReport.MissingNoteIndex = true;
+            ErrorReport.Count++;
+        }
+    }
+    
+    return {
+        "Notes": fileTable,
+        "Pages": foundFiles
+    };
 }
