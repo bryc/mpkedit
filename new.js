530afe6446e5864f9a0a17338aec40ab0717771a
. <linbrek@gmail.com>
2015-04-26 03:00:48 -0400

    changes to Note parsing - proper output format

--- new.js
+++ new.js
@@ -12,87 +12,102 @@ function MemPak()
         ref.data = data;
         ref.filename = "MemPak.mpk";
     }
-function parse(data)
-{
-    var i,p,p2,a,b,c,output,noteKeys=[];
 
-    function checkIndexes(o)
+    function parse(data)
     {
-        var seq,ends=0,Output={},found={keys:[],values:[],parsed:[]};
-        for(i = o + 0xA; i < o + 0x100; i += 2)
+        var i,p,p2,a,b,c,d0,d1,output,noteKeys=[],notes={};
+    
+        function checkIndexes(o)
         {
-            p  = data[i + 1]; p2 = data[i];
-
-            if(p2 !== 0 || p !== 1 && p !== 3 && p < 5 || p > 127)   
+            var seq,ends=0,Output={},found={keys:[],values:[],parsed:[]};
+            for(i = o + 0xA; i < o + 0x100; i += 2)
             {
-                return false;
-            }
-            else if(p2 === 0 && p === 1 || p !== 3 && p >= 5 && p <= 127)
-            {
-                if(p === 1) {ends += 1;}
-
-                if(p !== 1 && found.values.indexOf(p) > -1) {
+                p  = data[i + 1]; p2 = data[i];
+    
+                if(p2 !== 0 || p !== 1 && p !== 3 && p < 5 || p > 127)   
+                {
                     return false;
                 }
-                found.values.push(p);
-                found.keys.push((i - o) / 2);
-            }
-        }
-        found.indexes = found.keys.filter(function(n)
-        {
-            return found.values.indexOf(n) === -1;
-        });
-
-        if (noteKeys.length !== ends || noteKeys.length !== found.indexes.length) {
-            return false;
-        }
-        for (i = 0; i < noteKeys.length; i++) {
-            if (noteKeys.indexOf(found.indexes[i]) === -1) {
-                return false;
-            }
-        }
-
-        for(i = 0; i < found.indexes.length; i++)
-        {
-            seq = []; p = found.indexes[i];
-            while(p === 1 || p >= 5 && p <= 127)
-            {
-                if(p === 1)
+                else if(p2 === 0 && p === 1 || p !== 3 && p >= 5 && p <= 127)
                 {
-                    Output[found.indexes[i]] = seq;
-                    break;
+                    if(p === 1) {ends += 1;}
+    
+                    if(p !== 1 && found.values.indexOf(p) > -1) {
+                        return false;
+                    }
+                    found.values.push(p);
+                    found.keys.push((i - o) / 2);
                 }
-                seq.push(p);
-                found.parsed.push(p);
-                p = data[(p * 2) + o + 1];
             }
-        }
-        if (found.parsed.length !== found.keys.length) {
-            return false;
-        }
-        for (i = 0; i < found.parsed.length; i++) {
-            if (found.parsed.indexOf(found.keys[i]) === -1) {
+            found.indexes = found.keys.filter(function(n)
+            {
+                return found.values.indexOf(n) === -1;
+            });
+    
+            if (noteKeys.length !== ends || noteKeys.length !== found.indexes.length) {
                 return false;
             }
+            for (i = 0; i < noteKeys.length; i++) {
+                if (noteKeys.indexOf(found.indexes[i]) === -1) {
+                    return false;
+                }
+            }
+    
+            for(i = 0; i < found.indexes.length; i++)
+            {
+                seq = []; p = found.indexes[i];
+                while(p === 1 || p >= 5 && p <= 127)
+                {
+                    if(p === 1)
+                    {
+                        Output[found.indexes[i]] = seq;
+                        break;
+                    }
+                    seq.push(p);
+                    found.parsed.push(p);
+                    p = data[(p * 2) + o + 1];
+                }
+            }
+            if (found.parsed.length !== found.keys.length) {
+                return false;
+            }
+            for (i = 0; i < found.parsed.length; i++) {
+                if (found.parsed.indexOf(found.keys[i]) === -1) {
+                    return false;
+                }
+            }
+            return Output;
         }
-        return Output;
-    }
-
-    for(i = 0x300; i < 0x500; i += 32)
-    {
-        p = data[i + 0x07]; a = data[i + 0x06];
-        b = data[i + 0x0A]; c = data[i + 0x0B];
-
-        if(p>=5 && p<=127 && a===0 && b===0 && c===0)
+    
+        for(i = 0x300; i < 0x500; i += 32)
         {
-            noteKeys.push(p);  
+            p = data[i + 0x07]; a = data[i + 0x06];
+            b = data[i + 0x0A]; c = data[i + 0x0B];
+            d0 = data[i]+data[i+1]+data[i+2]+data[i+3];
+            d1 = data[i+4]+data[i+5];
+            
+            if(p>=5 && p<=127 && d0>0 && d1>0 && a===0 && b===0 && c===0)
+            {
+                noteKeys.push(p);  
+                notes[(i - 0x300) / 32] = {
+                    indexes: p
+                };
+            }
+        }
+    
+        output = checkIndexes(0x100) || checkIndexes(0x200);
+
+        if(output)
+        {
+            for(i = 0; i < Object.keys(notes).length; i++)
+            {
+                notes[Object.keys(notes)[i]].indexes = output[notes[Object.keys(notes)[i]].indexes];
+            }
+            return notes;
+        } else {
+            return false;
         }
     }
-
-    output = checkIndexes(0x100) || checkIndexes(0x200);
-    return output;
-}
-
 
     var ref   = this;
     ref.init  = init;
@@ -120,8 +135,12 @@ window.addEventListener("drop", function(event)
                 data = data.subarray(0x1040);
             }
             var parsedData = T.parse(data);
-            if(parsedData) { console.log(parsedData, event.target.name) } else { console.error("Invalid file: ", event.target.name); }
-            //console.log(T.MemPak);
+            if(parsedData) {
+                console.log("%c"+event.target.name,"font:bold 12px DejaVu Sans");
+                console.log(parsedData);
+                console.log("   ");
+            }
+            else { console.error("Invalid file: ", event.target.name); }
         };
         reader.readAsArrayBuffer(files[i].slice(0, 36928));
     }
