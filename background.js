df8ebde5f4830220ff2dfa58bab84886214e2d52
. <linbrek@gmail.com>
2015-07-15 22:12:58 -0400

    Update background.js

--- background.js
+++ background.js
@@ -1,7 +1,5 @@
-chrome.app.runtime.onLaunched.addListener(function()
-{
-    chrome.app.window.create("index.html",
-    {
+chrome.app.runtime.onLaunched.addListener(function() {
+    chrome.app.window.create("index.html", {
         bounds: {width: 512, height: 650},
         resizable: false
     });
