46267f88144f2c59f0db577034b8e3aae0c3e600
bryc <linbrek@gmail.com>
2015-08-15 11:42:05 -0400

    module.

--- mpkedit2.js
+++ mpkedit2.js
@@ -1,6 +1,14 @@
-var MPKEdit = {};
+var MPKEdit = (function MPKEdit() {
 
-MPKEdit.Parser = (function() {
+	function MPKEdit() {}
+	MPKEdit.prototype.elem = function elem(d) {
+		console.log(new Date());
+	};
+
+    return new MPKEdit();
+}());
+
+(function MPKParser(MPKEdit) {
 	function resize(b){for(var c=new Uint8Array(32768),a=0;a<b.length;a++)c[a]=b[a];return c};
 	function arrstr(b,a,c){b=b||[];c=c||b.length;var d="";for(a=a||0;a<c;a++)d+=String.fromCharCode(b[a]);return d};
 	var n64code = {0:"",15:" ",16:"0",17:"1",18:"2",19:"3",20:"4",21:"5",22:"6",23:"7",24:"8",25:"9",26:"A",27:"B",28:"C",29:"D",30:"E",31:"F",32:"G",33:"H",34:"I",35:"J",36:"K",37:"L",38:"M",39:"N",40:"O",41:"P",42:"Q",43:"R",44:"S",45:"T",46:"U",47:"V",48:"W",49:"X",50:"Y",51:"Z",52:"!",53:'"',54:"#",55:"'",56:"*",57:"+",58:",",59:"-",60:".",61:"/",62:":",63:"=",64:"?",65:"@",66:"。",67:"゛",68:"゜",69:"ァ",70:"ィ",71:"ゥ",72:"ェ",73:"ォ",74:"ッ",75:"ャ",76:"ュ",77:"ョ",78:"ヲ",79:"ン",80:"ア",81:"イ",82:"ウ",83:"エ",84:"オ",85:"カ",86:"キ",87:"ク",88:"ケ",89:"コ",90:"サ",91:"シ",92:"ス",93:"セ",94:"ソ",95:"タ",96:"チ",97:"ツ",98:"テ",99:"ト",100:"ナ",101:"ニ",102:"ヌ",103:"ネ",104:"ノ",105:"ハ",106:"ヒ",107:"フ",108:"ヘ",109:"ホ",110:"マ",111:"ミ",112:"ム",113:"メ",114:"モ",115:"ヤ",116:"ユ",117:"ヨ",118:"ラ",119:"リ",120:"ル",121:"レ",122:"ロ",123:"ワ",124:"ガ",125:"ギ",126:"グ",127:"ゲ",128:"ゴ",129:"ザ",130:"ジ",131:"ズ",132:"ゼ",133:"ゾ",134:"ダ",135:"ヂ",136:"ヅ",137:"デ",138:"ド",139:"バ",140:"ビ",141:"ブ",142:"ベ",143:"ボ",144:"パ",145:"ピ",146:"プ",147:"ペ",148:"ポ"};
@@ -15,7 +23,9 @@ MPKEdit.Parser = (function() {
 	function parse(a){a=new Uint8Array(a);"123-456-STD"===arrstr(a,0,11)&&(a=a.subarray(4160));if(!a||!1===checkHeader(a))return!1;var b=checkNotes(a),f=checkIndexes(a,256);if(f){for(var c=0,g=0,d=0;d<Object.keys(b).length;d++){var e=b[Object.keys(b)[d]];e.indexes=f[e.indexes];c+=e.indexes.length;g++}result.NoteTable=b;result.usedPages=c;result.usedNotes=g;result.data=a;return!0}return!1};
 	console.log("INFO: MPKEdit.Parser initialised");
 
-	return function MPKParser(data, filename) { 
+    MPKEdit.Parser = function(data, filename) {
+
+    	console.log(this.elem)
 		if(this.State.data && isNote(data)) {
 			this.State.import(data);
 		} else if(parse(data)) {
@@ -28,19 +38,19 @@ MPKEdit.Parser = (function() {
 		} else {
 			console.warn("ERROR: Data invalid: " + filename, this.State.$ || '');
 		}
-	};
-}());
+    };
+    
+}(MPKEdit));
 
-MPKEdit.State = (function() {
+(function MPKState(MPKEdit) {
 	function MPKState() {}
 	MPKState.prototype.import = function importNote(d){var e=new Uint8Array(this.data),f=d.subarray(0,32);d=d.subarray(32);var b=d.length/256;if(123>=this.usedPages+b&&16>this.usedNotes){for(var c=[],a=10;256>a&&c.length!==b;a+=2)3===e[256+a+1]&&c.push(a/2);f[6]=0;f[7]=c[0];for(a=0;a<c.length;a++){var g=256*c[a];e[256+(2*c[a]+1)]=a===c.length-1?1:c[a+1];for(b=0;256>b;b++)e[g+b]=d[256*a+b]}for(a=0;16>a;a++)if(void 0===this.NoteTable[a]){d=768+32*a;for(b=0;32>b;b++)e[d+b]=f[b];break}MPKEdit.Parser(e)}};
-	var out = new MPKState();
-	console.log("INFO: MPKEdit.State initialised");
-	return out;
-}());
 
-// Drag events *****************************************************************
-// *****************************************************************************
+    MPKEdit.State = new MPKState();
+	console.log("INFO: MPKEdit.State initialised");
+}(MPKEdit));
+
+
 window.ondragover=function(){return false}
 
 window.ondrop = function(e) {
