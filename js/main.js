81f781cbd7fedb982137d603d8bb474ec23280ad
bryc <linbrek@gmail.com>
2018-02-12 22:54:46 -0500

    indent

--- js/main.js
+++ js/main.js
@@ -23,20 +23,20 @@ var MPKEdit = (function MPKEdit() {
       checksum algo.
     */
     MPKEdit.cyrb32 = function cyb(key, seed = 3053) {
-    var m = 1540483477, h = seed ^ key.length;
-    for(var i=0,k,chunk=-4&key.length; i<chunk; i+=4) {
-        k = key[i+3]<<24 | key[i+2]<<16 | key[i+1]<<8 | key[i];
-        k = Math.imul(k, m), k ^= k >>> 24;
-        k = Math.imul(k, m), h = Math.imul(h, m) ^ k;
-    }
-    switch (3 & key.length) {
-        case 3: h ^= key[i + 2] << 16;
-        case 2: h ^= key[i + 1] << 8;
-        case 1: h ^= key[i], h = Math.imul(h, m);
-    }
-    h ^= h >>> 13, h = Math.imul(h, m), h ^= h >>> 15;
-    return h >>> 0;
-};
+        var m = 1540483477, h = seed ^ key.length;
+        for(var i=0,k,chunk=-4&key.length; i<chunk; i+=4) {
+            k = key[i+3]<<24 | key[i+2]<<16 | key[i+1]<<8 | key[i];
+            k = Math.imul(k, m), k ^= k >>> 24;
+            k = Math.imul(k, m), h = Math.imul(h, m) ^ k;
+        }
+        switch (3 & key.length) {
+            case 3: h ^= key[i+2] << 16;
+            case 2: h ^= key[i+1] << 8;
+            case 1: h ^= key[i], h = Math.imul(h, m);
+        }
+        h ^= h >>> 13, h = Math.imul(h, m), h ^= h >>> 15;
+        return h >>> 0;
+    };
 
     /* -----------------------------------------------
     function: MPKEdit.crc8(data)
